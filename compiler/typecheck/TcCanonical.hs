5bc717e9374c3d1c5b24fe676696f7a377e0068d
Richard Eisenberg <eir@cis.upenn.edu>
2014-12-12 10:40:18 -0500

    Bugfixing

--- compiler/typecheck/TcCanonical.hs
+++ compiler/typecheck/TcCanonical.hs
@@ -1593,7 +1593,8 @@ unifyWanted loc role    orig_ty1 orig_ty2
            ; return (mkTcTyConAppCo role funTyCon [co_s,co_t]) }
     go (TyConApp tc1 tys1) (TyConApp tc2 tys2)
       | tc1 == tc2, isDecomposableTyCon tc1, tys1 `equalLength` tys2
-      , not (isNewTyCon tc1) || role == Nominal -- don't look under newtypes!
+      , (not (isNewTyCon tc1) && not (isDataFamilyTyCon tc1)) || role == Nominal
+         -- don't look under newtypes!
       = do { cos <- zipWith3M (unifyWanted loc) (tyConRolesX role tc1) tys1 tys2
            ; return (mkTcTyConAppCo role tc1 cos) }
     go (TyVarTy tv) ty2
@@ -1637,6 +1638,7 @@ unify_derived loc role    orig_ty1 orig_ty2
            ; unify_derived loc role t1 t2 }
     go (TyConApp tc1 tys1) (TyConApp tc2 tys2)
       | tc1 == tc2, isDecomposableTyCon tc1, tys1 `equalLength` tys2
+      , (not (isNewTyCon tc1) && not (isDataFamilyTyCon tc1)) || role == Nominal
       = unifyDeriveds loc (tyConRolesX role tc1) tys1 tys2
     go (TyVarTy tv) ty2
       = do { mb_ty <- isFilledMetaTyVar_maybe tv

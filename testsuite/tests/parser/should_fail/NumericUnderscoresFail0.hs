1dab0295311c9744109754d4600a3e06f0249860
Takenobu Tani <takenobu.hs@gmail.com>
2017-10-21 11:05:35 +0900

    add testcase of failures / disallowed patterns for NumericUnderscores

---
+++ testsuite/tests/parser/should_fail/NumericUnderscoresFail0.hs
@@ -0,0 +1,21 @@
+{-# Language NumericUnderscores #-}
+
+main :: IO ()
+main = do
+    -- invalid case for NumericUnderscores extension
+    print [
+            -- integer
+            1000000_,
+            _1000000,
+
+            -- float
+            0_.0001,
+            _0.0001,
+            0.0001_,
+            0._0001,
+
+            -- float with exponent
+            1e_+23,
+            1e+23_,
+            1e+_23
+          ]

c99e7450bd417810b2a15ef42736fcee7d8b6fbe
Takenobu Tani <takenobu.hs@gmail.com>
2017-11-25 14:23:00 +0900

    split NumericUnderscore0 to 0 and 1

--- testsuite/tests/parser/should_fail/NumericUnderscoresFail1.hs
+++ testsuite/tests/parser/should_fail/NumericUnderscoresFail1.hs
@@ -7,7 +7,14 @@
 main :: IO ()
 main = do
     print [
-            -- integer
-            1000000_,
-            _1000000,
+            -- float
+            0_.0001,
+            _0.0001,
+            0.0001_,
+            0._0001,
+
+            -- float with exponent
+            1e_+23,
+            1e+23_,
+            1e+_23
           ]
